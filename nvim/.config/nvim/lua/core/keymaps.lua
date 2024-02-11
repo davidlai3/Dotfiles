07a4bef670d590bd67f01ac629d9a45b2a0dfc2d
David <lairdavid1@gmail.com>
2024-02-11 14:25:59 -0500

    Added more useful keymaps

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -11,4 +11,21 @@ vim.o.shiftwidth = 2   -- Number of spaces to use for autoindent
 vim.o.softtabstop = 2  -- Number of spaces for <Tab> and <BS> in insert mode
 vim.wo.number = true   -- Show line numbers    
 
+-- jk > ESC
+k("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
+-- Copy and paste using system
+-- k("", "<C-p>", "<C-r>+", {desc = "Use Control P to paste from system"})
+k("", "Y", "\"+y", {desc = "Use uppercase Y for system clip(motions work)"})
+
+-- Keeps text selected when shifting 
+k("v", ">", ">gv", {desc = "Selects again"})
+k("v", "<", "<gv", {desc = "Selects again"})
+k("x", ">", ">gv", {desc = "Selects again"})
+k("x", "<", "<gv", {desc = "Selects again"})
+
+-- Moves lines up and down
+k("v", "J", ":m '>+1<CR>gv=gv", {desc = "Stays in place during join"})
+k("v", "K", ":m '<-2<CR>gv=gv", {desc = "Stays in place during join"})
+k("x", "J", ":m '>+1<CR>gv=gv", {desc = "Stays in place during join"})
+k("x", "K", ":m '<-2<CR>gv=gv", {desc = "Stays in place during join"})
