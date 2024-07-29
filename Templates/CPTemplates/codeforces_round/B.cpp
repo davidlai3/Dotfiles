944dfb549be147783331d0101ee0cf9200840f9e
David <lairdavid1@gmail.com>
2024-07-29 01:29:58 -0400

    updated comp prog templates

--- Templates/CPTemplates/codeforces_round/B.cpp
+++ Templates/CPTemplates/codeforces_round/B.cpp
@@ -1,8 +1,15 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2")
 #include <bits/stdc++.h>    
 #define ll long long    
 #define pb push_back    
-#define mod 1000000007
+#define mp make_pair
+#define ii pair<int, int>
+#define fst first
+#define snd second
+#define MOD 1000000007
 using namespace std;    
+const int N = 1e5 + 5;
     
     
 void sol() {
@@ -12,6 +19,7 @@ void sol() {
     
 int main() {    
 	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 	int t = 1;    
 	cin >> t;    
 	while (t--) {    
--- Templates/CPTemplates/cpptemplate.cpp
+++ Templates/CPTemplates/cpptemplate.cpp
@@ -1,8 +1,15 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2")
 #include <bits/stdc++.h>    
 #define ll long long    
 #define pb push_back    
+#define mp make_pair
+#define ii pair<int, int>
+#define fst first
+#define snd second
 #define MOD 1000000007
 using namespace std;    
+const int N = 1e5 + 5;
     
     
 void sol() {
