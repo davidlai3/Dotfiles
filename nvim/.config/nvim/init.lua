445af9495097adb703770ec54f1db8e072a12c32
David <lairdavid1@gmail.com>
2025-02-13 12:09:55 -0500

    Automated indent folds

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,5 +5,7 @@ require("plugin_config")
 
 -- Relative line numbers
 vim.cmd([[set relativenumber]])
-
-
+-- Folding
+vim.cmd([[setlocal foldmethod=indent]])
+vim.cmd([[setlocal foldlevel=99]])
+vim.cmd([[setlocal foldnestmax=1]])
