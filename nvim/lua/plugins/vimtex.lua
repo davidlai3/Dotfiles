06d2065e4ac530e4c0d325e630794be8a7221bc9
David <lairdavid1@gmail.com>
2026-04-03 18:44:00 -0400

    Vimtex

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -1,9 +1,19 @@
 return {
     "lervag/vimtex",
-    lazy = false,     -- we don't want to lazy load VimTeX
-    -- tag = "v2.15", -- uncomment to pin to a specific release
+    lazy = false,
     init = function()
-        -- VimTeX configuration goes here, e.g.
         vim.g.vimtex_view_method = "zathura"
-    end
+
+        vim.g.vimtex_compiler_latexmk = {
+            executable = "latexmk",
+            options = {
+                "-pdf",
+                "-shell-escape",
+                "-interaction=nonstopmode",
+                "-synctex=1",
+                "-file-line-error",
+            },
+        }
+    end,
 }
+
